d3d3b3a3526c8f63114edd6037f69af740adffad
Ivan Figueredo <ivanfigueredo.et@gmail.com>
2022-11-15 10:00:57 +0100

    Fragment en la main activity

---
+++ app/src/main/java/com/example/primerproyecto/FirstFragment.kt
@@ -0,0 +1,13 @@
+package com.example.primerproyecto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
